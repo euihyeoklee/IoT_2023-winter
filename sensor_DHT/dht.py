53c9f162597b5af4ba471de039d83b0233da8be4
Euihyeok Lee <117284688+euihyeoklee@users.noreply.github.com>
2022-11-10 01:45:24 +0900

    code modification

--- sensor_DHT/dht.py
+++ sensor_DHT/dht.py
@@ -1,26 +1,32 @@
-# Adafruit_DHT 모듈을 사용하기 위해서는 사전에 설치가 필요
+# dht11 모듈을 사용하기 위해서는 사전에 설치가 필요
 # 터미널을 실행하여 아래 명령어를 입력하여 설치할 수 있음
-# pip3 install Adafruit_DHT
-
-import Adafruit_DHT as dht
+# pip install dht11
+import RPi.GPIO as GPIO
+import dht11
 import time
 
-# 아래에서 DHT11 센서의 경우 dht.DHT11, DHT22 센서의 경우 dht.DHT22 
-sensor = dht.DHT22
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+
+pin = 5
+sensor = dht11.DHT11(pin)
 
 # 라즈베리 파이의 GPIO 21번 핀이 DHT 센서의 데이터 핀(2번 핀)에 연결된 상태인 경우
-pin = 21
+
 
 try:
     while True:
-        # read_retry 함수는 2가지 값을 동시에 반환하는 함수. 첫번째 값이 습도, 두번째 값이 온도
-        humid, temp = dht.read_retry(sensor, pin)
-
-        if humid is not None and temp is not None:
-            print("Temp=", round(temp,2), " Humid=", round(humid,2))
+        # read 함수는 2가지 값을 동시에 반환하는 함수. 첫번째 값이 습도, 두번째 값이 온도
+        result = sensor.read()
+    
+        if result.is_valid():
+            print("Temperature: %-3.1f C" % result.temperature)
+            print("Humidity: %-3.1f %%" % result.humidity)
         else:
-            print("Failed to get reading")
-        time.sleep(0.5)
+            print("Error: %d" % result.error_code)
+        time.sleep(1)
         
 except KeyboardInterrupt:
     print("finished")
+
